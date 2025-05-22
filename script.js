6710c2face866c7fc2359c20f1c4b9dac5133b95
RamitaX19 <ramiro.dev19@gmail.com>
2025-05-22 14:46:06 -0300

    update 2.0

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
-//SLIDES
-const slides = document.querySelectorAll('.slide');
-let index = 0;
+const btn = document.getElementById("showMore");
+const extra = document.getElementById("extraImg");
+const showLess = document.getElementById("showLess");
+const gallery = document.getElementById("galeria1");
+const gallery1 = document.getElementById("galeria2");
 
-function showSlide(i) {
-    slides.forEach(slide => slide.classList.remove('show'));
-    slides[i].classList.add('show');
-}
+showMore.addEventListener("click", function () {
+    extraImg.classList.add("visible");
+    showMore.style.display = "none";
+    showLess.style.display = "inline-block";
+});
 
-document.querySelector('.prev').onclick = () => {
-    index = (index - 1 + slides.length) % slides.length;
-    showSlide(index);
-};
+showLess.addEventListener("click", function () {
+    extraImg.classList.remove("visible");
+    showMore.style.display = "inline-block";
+    showLess.style.display = "none";
+    gallery1.scrollIntoView({ behavior: "smooth" });
+});
+////////////////////////////////////////////////////////////////////////////////////////////////
+showMore2.addEventListener("click", function () {
+    extraImg2.classList.add("visible");
+    showMore2.style.display = "none";
+    showLess2.style.display = "inline-block";
+});
 
-document.querySelector('.next').onclick = () => {
-    index = (index + 1) % slides.length;
-    showSlide(index);
-};
-/////////////////////////////////////////////////////////
-const slidesP = document.querySelectorAll('#slider2 .slidePD');
-let indexP = 0;
-
-function showSlideP(i) {
-    slidesP.forEach(slide => slide.classList.remove('show'));
-    slidesP[i].classList.add('show');
-}
-
-document.querySelector('#slider2 .prevP').onclick = () => {
-    indexP = (indexP - 1 + slidesP.length) % slidesP.length;
-    showSlideP(indexP);
-};
-
-document.querySelector('#slider2 .nextP').onclick = () => {
-    indexP = (indexP + 1) % slidesP.length;
-    showSlideP(indexP);
-};
-
-showSlideP(indexP);
-
+showLess2.addEventListener("click", function () {
+    extraImg2.classList.remove("visible");
+    showMore2.style.display = "inline-block";
+    showLess2.style.display = "none";
+    gallery.scrollIntoView({ behavior: "smooth" });
+});
 //BOTON-LINK
 document.getElementById("botonWSP").addEventListener("click", function() {
     window.open("https://wa.me/5491144131253", "_blank");
@@ -56,4 +48,3 @@ document.getElementById("botonTK").addEventListener("click", function() {
 document.getElementById("botonMAPS").addEventListener("click", function() {
     window.open("https://www.google.com.ar/maps/place/Entretejidas+Lanas+y+Mercer%C3%ADa/@-31.9047886,-64.5714444,19.6z/data=!4m6!3m5!1s0x95d2b3a2e6c2dee7:0x7d4d6e557993c94a!8m2!3d-31.9048481!4d-64.571343!16s%2Fg%2F11w4tnjl8z?hl=es&entry=ttu&g_ep=EgoyMDI1MDUwMy4wIKXMDSoASAFQAw%3D%3D", "_blank");
 });
-
